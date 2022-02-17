eae0292f16992725814bb79e4d3db8d5ea393db7
SeppUnterwurzacher <alexander.gogl@gmx.at>
2022-02-17 20:54:59 +0100

    Ermittlung der Position funktioniert

--- carreratest.py
+++ carreratest.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2 import driver
 from threading import Timer
 from carreralib import ControlUnit
 cu = ControlUnit('/dev/serial0')
@@ -9,28 +10,91 @@ print("gestartet")
 class Driver:
     def __init__(self, address):
         self.address = address
-    timestamp_new = 0
-    lap_time = 0
-    lap = 0
+        self.name = address
+        self.timestamp_new = 0
+        self.lap_time = 0
+        self.total_time = 0
+        self.lap = 0
+        self.bestlap = 0
+        self.position_info = 0
+        self.position = 0
+        
+    def zeit(self, data):
+        self.timestamp_old = float(self.timestamp_new)
+        self.timestamp_new = float(data.timestamp)
 
+        if self.timestamp_old > 0:
+            self.lap_time = (self.timestamp_new - self.timestamp_old) / 1000
 
-drivers = [Driver(i) for i in range(0, )]
+            if self.bestlap > 0 and self.lap_time < self.bestlap:
+                self.bestlap = self.lap_time
+
+            self.lap += 1
+            
+            if self.lap > rms.max_lap:
+                rms.first = self.address
+
+            self.total_time = self.total_time + self.lap_time
+            self.position_info = (self.address, self.lap, self.total_time)
+        
+            """ print("Driver: " + str(self.address) + " - " + self.name)
+            print("Rundenzeit: " + str(self.lap_time))
+            print("Runde: " + str(self.lap))
+            print("Total: " + str(self.total_time)) """
+
+            print("Fahrer: " + str(self.position_info[0]) + ", Zeit: " + str(self.position_info[1]) + " Runden: " + str(self.position_info[2]))
+
+class RMS:
+    def __init__(self):
+        self.max_lap = 0
+        self.laps = [0, 0, 0]
+        self.times = [0, 0, 0]
+        self.zeiten = [0, 0, 0]
+        self.zeiten_sortiert = []
+        self.not_first_round = False
+        self.first = -1
+
+drivers = [Driver(i) for i in range(0, 3)]
+
+# Festlegen der Namen der Fahrer
+drivers[0].name = "Rot"
+drivers[1].name = "Gelb"
+drivers[2].name = "Blau"
+
+rms = RMS()
+
+def position():
+    if rms.not_first_round:
+        # Die Positions Infos je Fahrer werden zusammengefasst und sortiert
+        rms.zeiten_sortiert = []
+        for i in range(0, 3):
+            rms.zeiten[i] = drivers[i].position_info
+        
+        for x in rms.zeiten:
+            if isinstance(x, tuple)==True:
+                rms.zeiten_sortiert.append(x)
+        rms.zeiten_sortiert = sorted(rms.zeiten_sortiert, key = lambda item: (item[1]*-1, item[2]))
+        
+        reihung = 1
+        for x in rms.zeiten_sortiert:
+            drivers[x[0]].position = reihung
+            reihung += 1
+
+    rms.not_first_round = True
 
 while True:
     data = cu.request()
 
     if isinstance(data, ControlUnit.Timer):
         address = data.address
-        drivers[address].timestamp_old = float(drivers[address].timestamp_new)
-        drivers[address].timestamp_new = float(data.timestamp)
-        
+        drivers[address].zeit(data)
 
-        if drivers[address].timestamp_old > 0:
-            drivers[address].lap_time = (drivers[address].timestamp_new - drivers[address].timestamp_old) / 1000
-            drivers[address].lap += 1
+        position()
+        print("zeiten:" + str(rms.zeiten))
+        print("sortiert:" + str(rms.zeiten_sortiert))
         
-            print("Driver: " + str(address))
-            print("Rundenzeit: " + str(drivers[address].lap_time))
-            print("Runde: " + str(drivers[address].lap))
-        
- 
+        for i in range(0, 3):
+            print("Position " + str(i) + ": " + str(drivers[i].position))
+        #print("Runden:" + str(rms.laps))
+        #print("Runden:" + str(rms.times))
+        #print("Fuehrender:" + str(rms.first))
